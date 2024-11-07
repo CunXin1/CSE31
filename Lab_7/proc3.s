2c40788ef79d3b02f54cd3a72fc1ba6040629c42
Ruibo's Desktop <sunruibo5@gmail.com>
2024-11-07 00:04:53 -0800

    update

--- Lab_7/proc3.s
+++ Lab_7/proc3.s
@@ -3,8 +3,9 @@ x:  .word 2
 y:  .word 4
 z:  .word 6
 newline: .asciiz "\n"
-msg_pq: .asciiz "p + q: "
+pq: .asciiz "p + q: "
 
+.text
 MAIN:
         la $t0, x
         lw $s0, 0($t0)        # $s0 = x
@@ -64,14 +65,14 @@ FOO:
     move $s1, $v0             # $s1 = q
 
     # Calculate p + q
-    add $v0, $s0, $s1         # $v0 = p + q
+    add $t0, $s0, $s1         # $t0 = p + q
 
     # Print "p + q: %d\n"
     li $v0, 4                 # System call code for print string
-    la $a0, msg_pq
+    la $a0, pq
     syscall
 
-    move $a0, $v0             # Move result to $a0
+    move $a0, $t0             # Move result to $a0 for printing
     li $v0, 1                 # System call code for print integer
     syscall
 
@@ -80,32 +81,33 @@ FOO:
     la $a0, newline
     syscall
 
+    # Move the result back to $v0
+    move $v0, $t0             # $v0 = p + q (Return value)
+
     # Restore registers
     lw $ra, 12($sp)           # Restore return address
     lw $a0, 8($sp)            # Restore m ($a0)
     lw $a1, 4($sp)            # Restore n ($a1)
     lw $a2, 0($sp)            # Restore o ($a2)
-    addi $sp, $sp, 16         # Restore stack pointer
+    addi $sp, $sp, 16         
     jr $ra                    # Return to caller
-
 
 BAR:
     addiu $sp, $sp, -12       # Allocate stack space for $ra, $a0, and $a1
-    sw $ra, 8($sp)            # Save return address
-    sw $a0, 4($sp)            # Save argument 'a' ($a0)
-    sw $a1, 0($sp)            # Save argument 'b' ($a1)
+    sw $ra, 8($sp)            # Backup return address
+    sw $a0, 4($sp)            # Backup 'a' ($a0)
+    sw $a1, 0($sp)            # Backup 'b' ($a1)
 
     sub $v0, $a1, $a0         # Compute $v0 = b - a (signed subtraction)
     sllv $v0, $v0, $a2        # Shift $v0 left by c bits: $v0 = (b - a) << c
 
-    lw $a0, 4($sp)            # Restore argument 'a' ($a0)
-    lw $a1, 0($sp)            # Restore argument 'b' ($a1)
+    lw $a0, 4($sp)            # Restore 'a' ($a0)
+    lw $a1, 0($sp)            # Restore 'b' ($a1)
     lw $ra, 8($sp)            # Restore return address
-    addiu $sp, $sp, 12        # Restore stack pointer
+    addiu $sp, $sp, 12       
     jr $ra                    # Return to caller
 
 
 END:
-        # Exit program
         li $v0, 10            # System call code for exit
         syscall
