7b556f9102eda22f23f8f15f9773406fb2d0a7e1
Ruibo's Desktop <sunruibo5@gmail.com>
2024-11-06 23:42:25 -0800

    updates

--- Lab_7/proc3.s
+++ Lab_7/proc3.s
@@ -29,21 +29,52 @@ MAIN:   la $t0, x
         syscall
         j END
 
-FOO: 
-        addi $sp, $sp, -8
-        sw $ra, 8($sp)		# Backup ra
-        sw $a0, 0($sp)		# Backup a0
+FOO:
+        addi $sp, $sp, -12     
+        sw $ra, 8($sp)         # Backup return address
+        sw $a0, 4($sp)         # Backup a0 (m)
+        sw $a1, 0($sp)         # Backup a1 (n)
 
-        # m + 0
-        
+        # Compute bar(m + o, n + o, m + n)
+        addu $t0, $a0, $a2     # t0 = m + o
+        addu $t1, $a1, $a2     # t1 = n + o
+        addu $t2, $a0, $a1     # t2 = m + n
+        move $a0, $t0
+        move $a1, $t1
+        move $a2, $t2
+        jal BAR                # Call bar
+        move $s0, $v0          # p = result of bar
 
+        # Compute bar(m - o, n - m, n + n)
+        subu $t0, $a0, $a2     # t0 = m - o
+        subu $t1, $a1, $a0     # t1 = n - m
+        addu $t2, $a1, $a1     # t2 = n + n
+        move $a0, $t0
+        move $a1, $t1
+        move $a2, $t2
+        jal BAR                # Call bar
+        move $s1, $v0          # q = result of bar
 
+        # Calculate p + q
+        addu $v0, $s0, $s1     # v0 = p + q (Return value)
 
-
-
-
+        lw $ra, 8($sp)         # Restore return address
+        addi $sp, $sp, 12      # Restore stack pointer
+        jr $ra                 # Return to caller
 
 BAR:
+        addiu $sp, $sp, -12   
+        sw $ra, 8($sp)        # Backup return address
+        sw $a0, 4($sp)        # Backup a
+        sw $a1, 0($sp)        # Backup b
+    
+        subu $v0, $a1, $a0    # v0 = b - a
+        sllv $v0, $v0, $a2    # v0 = (b - a) << c
 
+        lw $a0, 4($sp)        # Restore a
+        lw $a1, 0($sp)        # Restore b
+        lw $ra, 8($sp)        # Restore return address
+        addiu $sp, $sp, 12    # Restore stack pointer
+        jr $ra                # Return to caller
 
 END:
